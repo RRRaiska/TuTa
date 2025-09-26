2d5cb7e3d6033a30542581f06c999729f56fdd26
RRRaiska <raisaheinola@gmail.com>
2025-09-26 18:38:21 +0300

    4. Tehtävät

---
+++ Tehtavat/Tehtava4.1.py
@@ -0,0 +1,2 @@
+# Kirjoita while-toistorakennetta käyttävä ohjelma, joka tulostaa kolmella jaolliset luvut väliltä 1..1000.
+
---
+++ Tehtavat/Tehtava4.2.py
@@ -0,0 +1,11 @@
+# Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä antaa negatiivisen tuumamäärän.
+# Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm.
+
+tuumamaara = int(input("Tämä ohjelma muuttaa tuumia senttimetreiksi. Anna tuumamäärä: "))
+
+tuuma = 2.54
+while tuumamaara >= 1 and <1:
+        print(f"Antamasi tuumamäärä on {tuuma * tuumamaara} senttimetreinä.")
+
+else:
+    print("Luku on negatiivinen.")
