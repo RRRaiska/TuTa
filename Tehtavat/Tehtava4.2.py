8afd6e90ce2d4b2f57e01ec37fe591347759fed8
RRRaiska <raisaheinola@gmail.com>
2025-09-28 17:29:27 +0300

    4.1 ja 4.2

--- Tehtavat/Tehtava4.2.py
+++ Tehtavat/Tehtava4.2.py
@@ -1,11 +1,14 @@
 # Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä antaa negatiivisen tuumamäärän.
 # Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm.
 
-tuumamaara = int(input("Tämä ohjelma muuttaa tuumia senttimetreiksi. Anna tuumamäärä: "))
-
+tuumat = int(input("Tämä ohjelma muuttaa tuumia senttimetreiksi. Anna tuumamäärä, jonka haluat muutettavan: "))
 tuuma = 2.54
-while tuumamaara >= 1 and <1:
-        print(f"Antamasi tuumamäärä on {tuuma * tuumamaara} senttimetreinä.")
+tuumamaara = tuuma * tuumat
+while tuumamaara >= 1:
+    print(f"Antamasi tuumamäärä on {tuumamaara} senttimetriä.")
+    tuumat = int(input("Tämä ohjelma muuttaa tuumia senttimetreiksi. Anna tuumamäärä, jonka haluat muutettavan: "))
+    tuumamaara = tuuma * tuumat
+    if tuumamaara <= 1:
+        print("Annoit negatiivisen luvun. Anna positiivinen luku.")
 
-else:
-    print("Luku on negatiivinen.")
+
