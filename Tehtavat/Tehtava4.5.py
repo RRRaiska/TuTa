c5ca01d909374cc47aeba1142c1355fa295ee8f9
RRRaiska <raisaheinola@gmail.com>
2025-09-28 18:19:54 +0300

    4.1 ja 4.2

--- Tehtavat/Tehtava4.5.py
+++ Tehtavat/Tehtava4.5.py
@@ -4,3 +4,12 @@
 # Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty.
 # (Oikea käyttäjätunnus on python ja salasana rules).
 
+kayttajatunnus = input("Anna käyttäjätunnus: ")
+salasana = input("Anna salasana: ")
+
+while salasana != "Python123!":
+    print(f"Pääsy evätty.")
+    salasana = input("Anna salasana: ")
+
+else:
+    print("Tervetuloa!")
