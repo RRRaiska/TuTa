f687b48a87b18a5460e9576a63c4de3d1db99d56
RRRaiska <raisaheinola@gmail.com>
2025-10-12 16:38:46 +0300

    5.3

--- Tehtavat/Tehtava5.3.py
+++ Tehtavat/Tehtava5.3.py
@@ -3,3 +3,11 @@
     # Esimerkiksi luku 13 on alkuluku, koska se voidaan jakaa vain luvuilla 1 ja 13 siten,
     # että jako menee tasan.
     # Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös luvulla 3 tai luvulla 7.
+
+kokoinaisluku = int(input("Anna kokonaisluku: "))
+
+if kokoinaisluku % kokoinaisluku != 0 and kokoinaisluku % 1 == 0:
+    print(f"{kokoinaisluku} ei ole alkuluku.")
+
+else:
+    print(f"{kokoinaisluku} on alkuluku.")
