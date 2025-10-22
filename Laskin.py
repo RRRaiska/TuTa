4fcdcd9e9256e8c08d2a519191c1538b8496e402
RRRaiska <raisaheinola@gmail.com>
2025-10-22 17:56:31 +0300

    laskin

--- Laskin.py
+++ Laskin.py
@@ -1,20 +1,26 @@
 print("-------Tervetuloa käyttämään laskinta!-------")
-print("Valitse mitä toimintoa haluat käyttää? \n A: Yhteenlasku \n B: Vähennyslasku"
-      "\n C: Kertolasku \n D: Jakolasku ")
-valinta = input("Valintasi (A - D): ").upper()
+while True:
+    print("\nValitse mitä toimintoa haluat käyttää? \n A: Yhteenlasku \n B: Vähennyslasku"
+          "\n C: Kertolasku \n D: Jakolasku ")
+    valinta = input("Valintasi (A - D), Q lopettaa: ").upper()
 
-#Lukuja voi sekoittaa keskenään, mutta lukuja ja merkkijoukkoa ei voi ilman, että sen castaa.
-a = float(input("Anna ensimmäinen luku: "))
-b = float(input("Anna toinen luku: "))
+    #while loopin katkaisu
+    if valinta == "Q":
+        print("Ohjelma lopetetaan, kiitos hei!")
+        break
 
-if valinta == "A":
-    print("Yhteenlasku:", a + b)
-elif valinta == "B":
-    print("Vähennyslasku:", a - b)
-elif valinta == "C":
-    print("Kertolasku:", a * b)
-elif valinta == "D":
-    print("Desimaalijakolasku", a / b)
-else:
-    print("Valitasi on virheellinen.")
+    #Lukuja voi sekoittaa keskenään, mutta lukuja ja merkkijoukkoa ei voi ilman, että sen castaa.
+    a = float(input("Anna ensimmäinen luku: "))
+    b = float(input("Anna toinen luku: "))
+
+    if valinta == "A":
+        print("Yhteenlasku:", a + b)
+    elif valinta == "B":
+        print("Vähennyslasku:", a - b)
+    elif valinta == "C":
+        print("Kertolasku:", a * b)
+    elif valinta == "D":
+        print("Desimaalijakolasku", a / b)
+    else:
+        print("Valitasi on virheellinen.")
 
