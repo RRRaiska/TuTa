db4d3d902ef3c35132767525a24a45d91616c4be
RRRaiska <raisaheinola@gmail.com>
2025-10-31 21:00:25 +0200

    6.3 tehtävä

--- Tehtavat/Tehtava6.3.py
+++ Tehtavat/Tehtava6.3.py
@@ -4,3 +4,16 @@
 # Muunnos on tehtävä aliohjelmaa hyödyntäen. Muuntamista jatketaan siihen saakka,
 # kunnes käyttäjä syöttää negatiivisen gallonamäärän.
 # Yksi gallona on 3,785 litraa.
+
+def bensan_maara(gallonat):
+    summa = gallonat * gallona
+    return summa
+
+gallona = 3.785
+gallonat = float(input("Anna gallonoiden määrä? "))
+
+while gallonat >= 0:
+    print(bensan_maara(gallonat))
+    print(float(input("Anna gallonoiden määrä? ")))
+if gallonat < 0:
+    print("Kiitos käynnistä.")
