7d7e07a568423561aee6e4dbe659e3f5baa84cbc
RRRaiska <raisaheinola@gmail.com>
2025-11-09 20:48:21 +0200

    7 tehtävien vienti

--- Tehtavat/Tehtava7.1.py
+++ Tehtavat/Tehtava7.1.py
@@ -4,3 +4,8 @@
 # vastaavat vuodenajat merkkijonoina monikkotietorakenteeseen.
 # Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten,
 # että joulukuu on ensimmäinen talvikuukausi.
+
+vuodenajat = {"talvi":"Joulukuu" "Tammikuu" "Helmikuu",
+            "kevät":"Maaliskuu" "Huhtikuu" "Toukokuu",
+            "kesä":"Kesäkuu" "Heinäkuu" "Elokuu",
+            "syksy":"Syyskuu" "Lokakuu" "Marraskuu"}
