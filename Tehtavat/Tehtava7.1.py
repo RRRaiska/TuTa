9f86e090c4159fb8b56b0586bfd9f9ef48b27de9
RRRaiska <raisaheinola@gmail.com>
2025-11-12 17:46:46 +0200

    7.1 tehtävä, kesken

--- Tehtavat/Tehtava7.1.py
+++ Tehtavat/Tehtava7.1.py
@@ -5,7 +5,20 @@
 # Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten,
 # että joulukuu on ensimmäinen talvikuukausi.
 
-vuodenajat = {"talvi":"Joulukuu" "Tammikuu" "Helmikuu",
-            "kevät":"Maaliskuu" "Huhtikuu" "Toukokuu",
-            "kesä":"Kesäkuu" "Heinäkuu" "Elokuu",
-            "syksy":"Syyskuu" "Lokakuu" "Marraskuu"}
+
+kuukaudet = ("Tammikuu","Helmikuu", "Maaliskuu" , "Huhtikuu" , "Toukokuu", "Kesäkuu" , "Heinäkuu" , "Elokuu", "Syyskuu" , "Lokakuu" , "Marraskuu","Joulukuu")
+
+kuukaudenNumero = int(input("Kerro kuukauden numero (1-12)? "))
+kuukausi = kuukaudet[kuukaudenNumero-1]
+
+if kuukaudenNumero == 11 or kuukaudenNumero == 0 or kuukaudenNumero == 1:
+    print("Vuodenaika on talvi.")
+if kuukaudenNumero == 2 or kuukaudenNumero == 3 or kuukaudenNumero == 4:
+    print("Vuodenaika on kevät.")
+if kuukaudenNumero == 5 or kuukaudenNumero == 6 or kuukaudenNumero == 7:
+    print("Vuodenaika on kesä.")
+if kuukaudenNumero == 8 or kuukaudenNumero == 9 or kuukaudenNumero == 10:
+    print("Vuodenaika on syksy.")
+
+
+
