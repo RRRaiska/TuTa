41cd3edc9f1a035a1ee5313fa7e60056e60ce955
RRRaiska <raisaheinola@gmail.com>
2025-11-18 19:09:15 +0200

    7.1. valmis

--- Tehtavat/Tehtava7.1.py
+++ Tehtavat/Tehtava7.1.py
@@ -6,20 +6,20 @@
 # että joulukuu on ensimmäinen talvikuukausi.
 
 
-kuukaudet = ("Tammikuu","Helmikuu", "Maaliskuu" , "Huhtikuu" , "Toukokuu", "Kesäkuu" , "Heinäkuu" , "Elokuu", "Syyskuu" , "Lokakuu" , "Marraskuu","Joulukuu")
+kuukaudet = ("Tammikuu","Helmikuu", "Maaliskuu" , "Huhtikuu" , "Toukokuu", "Kesäkuu" , "Heinäkuu" , "Elokuu", "Syyskuu" , "Lokakuu" , "Marraskuu", "Joulukuu")
 
 kuukaudenNumero = int(input("Kerro kuukauden numero (1-12)? "))
 kuukausi = kuukaudet[kuukaudenNumero-1]
 
-if kuukaudenNumero == 11 or kuukaudenNumero == 0 or kuukaudenNumero == 1:
-    print("Vuodenaika on talvi.")
-if kuukaudenNumero == 2 or kuukaudenNumero == 3 or kuukaudenNumero == 4:
-    print("Vuodenaika on kevät.")
-if kuukaudenNumero == 5 or kuukaudenNumero == 6 or kuukaudenNumero == 7:
-    print("Vuodenaika on kesä.")
-if kuukaudenNumero == 8 or kuukaudenNumero == 9 or kuukaudenNumero == 10:
-    print("Vuodenaika on syksy.")
+if kuukaudenNumero == 12 or kuukaudenNumero == 1 or kuukaudenNumero == 2:
+    print(kuukausi,f" {kuukausi}n vuodenaika on talvi.")
+if kuukaudenNumero == 3 or kuukaudenNumero == 4 or kuukaudenNumero == 5:
+    print(kuukausi,f" {kuukausi}n vuodenaika on kevät.")
+if kuukaudenNumero == 6 or kuukaudenNumero == 7 or kuukaudenNumero == 8:
+    print(kuukausi,f" {kuukausi}n vuodenaika on kesä.")
+if kuukaudenNumero == 9 or kuukaudenNumero == 10 or kuukaudenNumero == 11:
+    print(kuukausi,f" {kuukausi}n vuodenaika on syksy.")
 
-#ei toimi. ajatus ei kulje.
+
 
 
